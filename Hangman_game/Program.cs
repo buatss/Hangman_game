23f81c5981983d338edceb943478f5b1a07a9a0f
buats <buats@DESKTOP-47KFUI5>
2021-07-01 18:46:28 +0200

    Dodaj pliki projektów.

---
+++ Hangman_game/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hangman_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hangman game!");
+            string[] list = System.IO.File.ReadAllLines(@"C:\Users\buats\OneDrive\Pulpit\Motorola Academy\countries_and_capitals.txt");
+
+            int X = list.GetLength(0);
+          
+            Console.WriteLine($"Length: {X}");
+            string[] row = list[1].Split('|');
+
+            foreach (var word in row)
+            {
+                System.Console.WriteLine($"{word}");
+            }
+
+
+        }
+    }
+}
