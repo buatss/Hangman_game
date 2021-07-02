4cac7c91f8fec13cacfd96b5ed7318a8d2714a5f
buatss <buatss@gmail.com>
2021-07-02 15:58:41 +0200

    class extraction to another file

---
+++ Hangman_game/Hangman.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Hangman_game
+{
+    public class Hangman
+    {
+        int HP = 5;
+        string Capital;
+        string Country;
+        void Substract_HP(int amount)
+        {
+            HP = HP - amount;
+            if (HP > 0)
+            {
+                Console.WriteLine($"You lost {amount} health point(s)");
+            }
+            else
+            {
+                Console.WriteLine($"You lost");
+            }
+        }
+        public void get_attributes(string Cp,string Cn)
+        {
+            Capital = Cp;
+            Country = Cn;
+        }
+       public void check(string word)
+        {
+            if (word.Equals(Capital))
+            {
+                Console.WriteLine($"Congratulations! {Capital} is capital of {Country}");
+            }
+            else if(Capital.Contains(word))
+                {
+                Console.WriteLine($"Capital contains this letter");
+            }
+            else if (word.Length>1)
+            {
+                Substract_HP(2);
+            }
+            else if(word.Length==1)
+            {
+                Substract_HP(1);
+            }
+        }
+    }
+}
