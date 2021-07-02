1df6deae7fe148299f87be255255bad4acd44921
buatss <buatss@gmail.com>
2021-07-02 16:40:43 +0200

    Revert "typo"
    This reverts commit 27333b7b5e9a50459e596d322e7464c34911722b.

--- Hangman_game/Hangman.cs
+++ Hangman_game/Hangman.cs
@@ -4,8 +4,8 @@ namespace Hangman_game
 {
     public class Hangman
     {
-        private int _hp = 5; 
-        public string Capitol { get; private set; }
+        private int _hp = 5;
+        public string Capital { get; private set; }
         public string Country { get; private set; }
 
         public void SubstractHP(int amount)
@@ -23,19 +23,19 @@ namespace Hangman_game
 
         public void SetAttributes(string capitol, string country)
         {
-            this.Capitol = capitol;
+            this.Capital = capitol;
             this.Country = country;
         }
 
         public void Check(string word)
         {
-            if (word.Equals(Capitol))
+            if (word.Equals(Capital))
             {
-                Console.WriteLine($"Congratulations! {Capitol} is capitol of {Country}");
+                Console.WriteLine($"Congratulations! {Capital} is capital of {Country}");
             }
-            else if (Capitol.Contains(word))
+            else if (Capital.Contains(word))
             {
-                Console.WriteLine($"Capitol contains this letter");
+                Console.WriteLine($"Capital contains this letter");
             }
             else if (word.Length > 1)
             {
